d871032ea03a1521a2d946714707db0b1aa5a709
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:05:35 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,11 @@
+import sys
 
+argc = len(sys.argv)
+if (argc == 1):
+	print("none")
+if (argc != 1):
+	print(f"parameters: {argc - 1}")
+	i = 1
+	while (i != (argc)):
+		print(f"{sys.argv[i]}: {len(sys.argv[i])}")
+		i += 1
