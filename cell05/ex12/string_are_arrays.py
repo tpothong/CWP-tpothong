7a81f28c3014701baa18cd6d5a496fe283a13603
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:10:50 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,12 @@
+import sys
 
+if len(sys.argv) != 2:
+    print("none")
+else:
+    text = sys.argv[1]
+    z_count = text.count('z')
+    
+    if z_count == 0:
+        print("none")
+    else:
+        print("z" * z_count)
