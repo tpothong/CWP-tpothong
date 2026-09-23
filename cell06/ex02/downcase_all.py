67a559de91fd07b0f2131572212389082f26f0bb
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:29:18 +0700

    Update downcase_all.py

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,1 +1,12 @@
+import sys
 
+def downcase_it(text):
+    return text.lower()
+
+params = sys.argv[1:]
+
+if not params:
+    print("none")
+else:
+    for param in params:
+        print(downcase_it(param))
