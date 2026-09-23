0d690f702eb2a6686f78e3129656021f31dde7cc
tpothong <67011460@kmitl.ac.th>
2026-09-23 11:56:24 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+print("Thank you!")
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {int(first_number / second_number)}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
