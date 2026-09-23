bf2d27f073a1b8fd57357528d9429a11aecbcec4
tpothong <67011460@kmitl.ac.th>
2026-09-23 11:43:07 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+x = int(input(" "))
+if x == 0:
+    print("This number is both positive and negative.")
+elif x > 0:
+     print("This number is positive.")
+else:
+      print("This number is negative.")
