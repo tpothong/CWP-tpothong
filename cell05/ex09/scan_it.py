70efd1a94beef02040c082e6010149912269e75b
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:03:57 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,11 @@
+import sys, re
 
+argc = len(sys.argv)
+if (argc != 3):
+	print("none")
+if (argc == 3):
+	kc = len(re.findall(sys.argv[1], sys.argv[2]))
+	if (kc == 0):
+		print("none")
+	if (kc != 0):
+		print(f"{len(re.findall(sys.argv[1], sys.argv[2]))}")
