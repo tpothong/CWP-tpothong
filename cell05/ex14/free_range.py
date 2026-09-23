c9b403717130a48ffefdd25f92edf3206e8393a4
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:15:16 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,9 @@
+import sys
 
+if len(sys.argv) != 3:
+    print("none")
+else:
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    step = 1 if start <= end else -1
+    print(list(range(start, end + step, step)))
