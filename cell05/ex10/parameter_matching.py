45f1c220f416c3410224c5741b5c5103d35169ff
tpothong <67011460@kmitl.ac.th>
2026-09-23 21:04:48 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,10 @@
+import sys
 
+if (len(sys.argv) != 2):
+	print("Nope, sorry...")
+if (len(sys.argv) == 2):
+	s = input("What was the parameter? ").strip()
+	if (s == sys.argv[1]):
+		print("Good job!")
+	if (s != sys.argv[1]):
+		print("Nope, sorry...")
